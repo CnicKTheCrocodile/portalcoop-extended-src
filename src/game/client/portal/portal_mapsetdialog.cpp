72c4ff8d175ccd6777519f3c646f8bc59de70793
Mandrew <Wonderland_War@outlook.com>
2026-02-26 01:26:11 -0600

    Remove CMapSetItemPanel

--- src/game/client/portal/portal_mapsetdialog.cpp
+++ src/game/client/portal/portal_mapsetdialog.cpp
@@ -10,154 +10,6 @@ static CMapSetDialog *g_pMapSetDialog = NULL;
 using namespace vgui;
 
 //-----------------------------------------------------------------------------
-// Purpose: selectable item with screenshot for an individual chapter in the dialog
-//-----------------------------------------------------------------------------
-class CMapSetItemPanel : public EditablePanel
-{
-	DECLARE_CLASS_SIMPLE( CMapSetItemPanel, EditablePanel );
-
-	ImagePanel *m_pLevelPicBorder;
-	ImagePanel *m_pLevelPic;
-	//Label *m_pChapterLabel;
-	Label *m_pChapterNameLabel;
-
-	Color m_TextColor;
-	Color m_DisabledColor;
-	Color m_SelectedColor;
-	Color m_FillColor;
-
-	char m_szMapSetName[_MAX_PATH];
-	char m_szSelectedMap[_MAX_PATH];
-	int m_iRequiredPlayers;
-
-	bool m_bTeaserChapter;
-	bool m_bHasBonus;
-
-	bool m_bIsSelected;
-
-public:
-	CMapSetItemPanel( CMapSetDialog *parent, const char *name, const char *chapterName,
-						int chapterIndex, const char *mapsetName,
-						int iRequiredPlayers, const char *mapname ) : BaseClass( parent, name )
-	{
-		Q_strncpy( m_szMapSetName, mapsetName, sizeof(m_szMapSetName) );
-		Q_strncpy( m_szSelectedMap, mapname, sizeof(m_szSelectedMap) );		
-
-		m_pLevelPicBorder = SETUP_PANEL( new ImagePanel( this, "LevelPicBorder" ) );
-		m_pLevelPic = SETUP_PANEL( new ImagePanel( this, "LevelPic" ) );
-		m_bIsSelected = false;
-
-		m_iRequiredPlayers = iRequiredPlayers;
-
-		//m_pChapterLabel = new Label( this, "ChapterLabel", text );
-		m_pChapterNameLabel = new Label( this, "ChapterNameLabel", chapterName );
-
-		SetPaintBackgroundEnabled( false );
-
-		// the image has the same name as the config file
-		char szMaterial[ MAX_PATH ];
-		Q_snprintf( szMaterial, sizeof(szMaterial), "mapsets/%s", mapsetName );
-		char *ext = strstr( szMaterial, "." );
-		if ( ext )
-		{
-			*ext = 0;
-		}
-		m_pLevelPic->SetImage( szMaterial );
-
-		KeyValues *pKeys = NULL;
-
-		LoadControlSettings( "Resource/MapSetDialogItemPanel.res", NULL, pKeys );
-
-		int px, py;
-		m_pLevelPicBorder->GetPos( px, py );
-		SetSize( m_pLevelPicBorder->GetWide(), py + m_pLevelPicBorder->GetTall() );
-
-		// create a selection panel the size of the page
-		//CSelectionOverlayPanel *overlay = new CSelectionOverlayPanel( this, parent, chapterIndex );
-		//overlay->SetBounds(0, 0, GetWide(), GetTall());
-		//overlay->MoveToFront();
-
-		// HACK: Detect new episode teasers by the "Coming Soon" text
-		wchar_t w_szStrTemp[256];
-		m_pChapterNameLabel->GetText( w_szStrTemp, sizeof(w_szStrTemp)  );
-		m_bTeaserChapter = !wcscmp(w_szStrTemp, L"Coming Soon");
-		m_bHasBonus = false;
-	}
-
-	virtual void ApplySchemeSettings( IScheme *pScheme )
-	{
-		m_TextColor = pScheme->GetColor( "NewGame.TextColor", Color(255, 255, 255, 255) );
-		m_FillColor = pScheme->GetColor( "NewGame.FillColor", Color(255, 255, 255, 255) );
-		m_DisabledColor = pScheme->GetColor( "NewGame.DisabledColor", Color(255, 255, 255, 255) );
-		m_SelectedColor = pScheme->GetColor( "NewGame.SelectionColor", Color(255, 255, 255, 255) );
-
-		BaseClass::ApplySchemeSettings( pScheme );
-
-		// Hide chapter numbers for new episode teasers
-		//if ( m_bTeaserChapter )
-		//{
-			//m_pChapterLabel->SetVisible( false );
-		//}
-	}
-
-	bool IsSelected( void ) const { return m_bIsSelected; }
-
-	void SetSelected( bool state )
-	{
-		m_bIsSelected = state;
-
-		// update the text/border colors
-		if ( !IsEnabled() )
-		{
-			//m_pChapterLabel->SetFgColor( m_DisabledColor );
-			m_pChapterNameLabel->SetFgColor( Color(0, 0, 0, 0) );
-			m_pLevelPicBorder->SetFillColor( m_DisabledColor );
-			m_pLevelPic->SetAlpha( 128 );
-			return;
-		}
-
-		if ( state )
-		{
-			//m_pChapterLabel->SetFgColor( m_SelectedColor );
-			m_pChapterNameLabel->SetFgColor( m_SelectedColor );
-			m_pLevelPicBorder->SetFillColor( m_SelectedColor );
-		}
-		else
-		{
-			//m_pChapterLabel->SetFgColor( m_TextColor );
-			m_pChapterNameLabel->SetFgColor( m_TextColor );
-			m_pLevelPicBorder->SetFillColor( m_FillColor );
-		}
-		m_pLevelPic->SetAlpha( 255 );
-	}
-
-	const char *GetMapSetName()
-	{
-		return m_szMapSetName;
-	}
-
-	const char *GetSelectedMap()
-	{
-		return m_szSelectedMap;
-	}
-	
-	int GetRequiredPlayers()
-	{
-		return m_iRequiredPlayers;
-	}
-
-	bool IsTeaserChapter()
-	{
-		return m_bTeaserChapter;
-	}
-
-	bool HasBonus()
-	{
-		return m_bHasBonus;
-	}
-};
-
-//-----------------------------------------------------------------------------
 // Purpose: Constructor
 //-----------------------------------------------------------------------------
 CMapSetDialog::CMapSetDialog(Panel *parent, const char *panelName) : Frame(parent, panelName)
@@ -232,6 +84,7 @@ void CMapSetDialog::ApplySchemeSettings( vgui::IScheme *pScheme )
 
 void CMapSetDialog::ParseMapSetKeyValues( KeyValues *mapsets, int &i )
 {
+	return; // Disabled until the map set list has proper panels to add
 	// add chapters to combobox
 	for ( KeyValues *mapset = mapsets->GetFirstSubKey(); mapset != NULL; mapset = mapset->GetNextKey() )
 	{
@@ -240,11 +93,11 @@ void CMapSetDialog::ParseMapSetKeyValues( KeyValues *mapsets, int &i )
 
 		int required_players = mapset->GetInt( "required_players" );
 		const char *map = mapset->GetString( "map" );
-		CMapSetItemPanel *chapterPanel = SETUP_PANEL( new CMapSetItemPanel( this, NULL, chapterName, i, mapset->GetName(), required_players, map ) );
-		chapterPanel->SetVisible( true );
-		chapterPanel->InvalidateLayout( true );
+		//CMapSetItemPanel *chapterPanel = SETUP_PANEL( new CMapSetItemPanel( this, NULL, chapterName, i, mapset->GetName(), required_players, map ) );
+		//chapterPanel->SetVisible( true );
+		//chapterPanel->InvalidateLayout( true );
 
-		m_pMapSetList->AddItem( NULL, chapterPanel );
+		//m_pMapSetList->AddItem( NULL, chapterPanel );
 
 		++i;
 	}
